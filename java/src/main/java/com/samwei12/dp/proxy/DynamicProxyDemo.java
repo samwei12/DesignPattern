5b69c0b33ff58a905dd1e328c60d86b0fdb3ba20
samwei12 <dongxiaosen@icloud.com>
2021-07-31 21:09:02 +0800

    add proxy

---
+++ java/src/main/java/com/samwei12/dp/proxy/DynamicProxyDemo.java
@@ -0,0 +1,41 @@
+package com.samwei12.dp.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author samwei12
+ * @date 2021/7/31 8:42 下午
+ * @email dongxiaosen@icloud.com
+ */
+public class DynamicProxyDemo {
+    public static void main(String[] args) {
+        final NikeClothesFactory nikeClothesFactory = new NikeClothesFactory();
+        final ClothesFactory proxyInstance = (ClothesFactory) ProxyFactory.getProxyInstance(nikeClothesFactory);
+        proxyInstance.produce();
+    }
+}
+
+class ProxyFactory {
+    public static Object getProxyInstance(Object target) {
+        final MyInvocationHandler myInvocationHandler = new MyInvocationHandler(target);
+        return Proxy.newProxyInstance(target.getClass().getClassLoader(), target.getClass().getInterfaces(), myInvocationHandler);
+    }
+}
+
+class MyInvocationHandler implements InvocationHandler {
+    private final Object target;
+
+    public MyInvocationHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("代理类执行事前准备");
+        final Object invoke = method.invoke(target, args);
+        System.out.println("代理类执行事后准备");
+        return invoke;
+    }
+}
---
+++ java/src/main/java/com/samwei12/dp/proxy/StaticProxyDemo.java
@@ -0,0 +1,41 @@
+package com.samwei12.dp.proxy;
+
+/**
+ * @author samwei12
+ * @date 2021/7/31 8:38 下午
+ * @email dongxiaosen@icloud.com
+ */
+public class StaticProxyDemo {
+    public static void main(String[] args) {
+        final ClothesFactory nikeClothesFactory = new NikeClothesFactory();
+        final ClothesFactory proxyClothesFactory = new ProxyClothesFactory(nikeClothesFactory);
+        proxyClothesFactory.produce();
+    }
+}
+
+interface ClothesFactory {
+    void produce();
+}
+
+class ProxyClothesFactory implements ClothesFactory {
+    // 被代理对象
+    private ClothesFactory clothesFactory;
+
+    public ProxyClothesFactory(ClothesFactory clothesFactory) {
+        this.clothesFactory = clothesFactory;
+    }
+
+    @Override
+    public void produce() {
+        System.out.println("代理类事前准备工作");
+        clothesFactory.produce();
+        System.out.println("代理类时候准备工作");
+    }
+}
+
+class NikeClothesFactory implements ClothesFactory {
+    @Override
+    public void produce() {
+        System.out.println("Nike 制衣过程");
+    }
+}
